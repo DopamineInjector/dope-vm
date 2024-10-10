0bef6967ebc2199c5f3b1971d334e8515d1fc059
tpalinski <tpalinski@icloud.com>
2024-10-10 09:48:17 +0200

    [CU-86964qtxt] WASM vm runtime

--- dopechain-vm/src/main.rs
+++ dopechain-vm/src/main.rs
@@ -1,3 +1,16 @@
+use std::{env::args, process::exit};
+
+use dopechain_vm::run_binary;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    if args.len() != 2 {
+        println!("Error while trying to run dopechain-vm: invalid argument count");
+        exit(1);
+    }
+    let binary_path = args.get(1).unwrap();
+    let res = run_binary(binary_path);
+    if let Err(err) = res {
+        println!("Error while running dopechain-vm: {err}");
+    }
 }
